3d662e10cbc07946083a870f100749c0fd284046
khaangnguyeen <78076796+khengyun@users.noreply.github.com>
2023-02-13 17:14:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 {
-url: "https://github.com/khengyun/promotion/",
-text: "Fcoder"
+  "url": "https://neeva.com/index?pk_source=chromegptextension&pk_medium=ad&pk_campaign=0203_2030UTC8",
+  "image": { "url": "https://i.postimg.cc/Vk8DPhsV/neevajpg.jpg", "size": 50 },
+  "title": "Neeva.ai",
+  "text": "Unlock AI-powered search results with a free Neeva.ai account today.",
+  "footer": {
+    "url": "https://neeva.com/index?pk_source=chromegptextension&pk_medium=ad&pk_campaign=0203_2030UTC8",
+    "text": "neeva.ai"
+  },
+  "label": {
+    "url": "https://chatgpt4google.notion.site/Advertise-on-ChatGPT-for-Google-8a7b72f93c4e4dfcae89ec1343f1e534",
+    "text": "Promotion"
+  }
 }
