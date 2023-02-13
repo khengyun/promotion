591722fe01c863a7f058e15ab41beb504f1664d4
khaangnguyeen <78076796+khengyun@users.noreply.github.com>
2023-02-13 17:35:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 {
-  "url": "https://neeva.com/index?pk_source=chromegptextension&pk_medium=ad&pk_campaign=0203_2030UTC8",
-  "image": { "url": "https://i.postimg.cc/Vk8DPhsV/neevajpg.jpg", "size": 50 },
-  "title": "Neeva.ai",
-  "text": "Unlock AI-powered search results with a free Neeva.ai account today.",
+  "url": "https://www.facebook.com/photo/?fbid=584175026843692&set=a.517487573512438",
+  "image": { "url": "https://scontent.fvca1-4.fna.fbcdn.net/v/t39.30808-6/315547239_584175020177026_1510743364023863452_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=8LkamwDGk3kAX_qV2er&_nc_ht=scontent.fvca1-4.fna&oh=00_AfCiZBgZbChsSAnvd1Doap_wopfCng3_H90qafhEPOEiJg&oe=63EFE603", "size": 50 },
+  "title": "FCoder - Hovilo",
+  "text": "CLB đỉnh cao dũ chụ",
   "footer": {
-    "url": "https://neeva.com/index?pk_source=chromegptextension&pk_medium=ad&pk_campaign=0203_2030UTC8",
-    "text": "neeva.ai"
+    "url": "https://www.facebook.com/FCoderFUCT",
+    "text": "khengyun"
   },
   "label": {
-    "url": "https://chatgpt4google.notion.site/Advertise-on-ChatGPT-for-Google-8a7b72f93c4e4dfcae89ec1343f1e534",
+    "url": "https://www.facebook.com/FCoderFUCT",
     "text": "Promotion"
   }
 }
