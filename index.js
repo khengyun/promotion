0ecebd5e479db02e4c64762e2e4f3ce9f33b0cc0
khaangnguyeen <78076796+khengyun@users.noreply.github.com>
2023-03-07 17:51:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 {
   "url": "https://www.facebook.com/photo/?fbid=584175026843692&set=a.517487573512438",
-  "image": { "url": "https://scontent.fvca1-4.fna.fbcdn.net/v/t39.30808-6/315547239_584175020177026_1510743364023863452_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=8LkamwDGk3kAX_qV2er&_nc_ht=scontent.fvca1-4.fna&oh=00_AfCiZBgZbChsSAnvd1Doap_wopfCng3_H90qafhEPOEiJg&oe=63EFE603", "size": 50 },
+  "image": { "url": "https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-6/315547239_584175020177026_1510743364023863452_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=omhu1yq0-8oAX9HNSnq&_nc_ht=scontent.fsgn5-6.fna&oh=00_AfC2bqt_4GWR26rZP-8nXAar0Ipf56U3x4TUpJm-_DcqIA&oe=640B9583", "size": 50 },
   "title": "FCoder - Hovilo",
   "text": "CLB đỉnh cao dũ chụ",
   "footer": {
