ef306488cc1802ebf44833cdfd6162ceba63c031
khaangnguyeen <78076796+khengyun@users.noreply.github.com>
2023-03-07 18:08:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 {
-  "url": "https://www.facebook.com/photo/?fbid=584175026843692&set=a.517487573512438",
-  "image": { "url": "https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-6/315547239_584175020177026_1510743364023863452_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=omhu1yq0-8oAX9HNSnq&_nc_ht=scontent.fsgn5-6.fna&oh=00_AfC2bqt_4GWR26rZP-8nXAar0Ipf56U3x4TUpJm-_DcqIA&oe=640B9583", "size": 50 },
+  "url": "https://www.facebook.com/FCoderFUCT",
+  "image": { "url": "https://user-images.githubusercontent.com/78076796/223402144-32477372-1e97-4ad2-a865-dbc155d4373b.png", "size": 50 },
   "title": "FCoder - Hovilo",
   "text": "CLB đỉnh cao dũ chụ",
   "footer": {
@@ -8,7 +8,7 @@
     "text": "khengyun"
   },
   "label": {
-    "url": "https://www.facebook.com/FCoderFUCT",
+    "url": "https://khenguyn.notion.site/promotion_edunext-bd80a939e2d54cf982e189f336c5a04f",
     "text": "Promotion"
   }
 }
